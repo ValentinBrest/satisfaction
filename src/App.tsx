0e5f6f5444e8b93b16273efd5410cc7404ec99bc
valk <Valiantsin.Kanapatski@epolsoft.com>
2023-02-06 16:51:50 +0300

    feat: devide into chunks using lazy()

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
+import { Suspense } from "react"
 import { Link, Route, Routes } from "react-router-dom"
 import './index.scss'
-import AboutPage from "./pages/AboutPage/AboutPage"
-import MainPage from "./pages/MainPage/MainPage"
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async"
+import { MainPageAsync } from "./pages/MainPage/MainPage.async"
 
 function App() {
     return (
         <div className="app">
             <Link to="/about">О сайте</Link>
             <Link to="/main">Главная</Link>
+            <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path='/about' element={<AboutPage/>}/>
-                    <Route path='/main' element={<MainPage/>}/>
+                    <Route path='/about' element={<AboutPageAsync/>}/>
+                    <Route path='/main' element={<MainPageAsync/>}/>
                 </Routes>
+            </Suspense>
+                
         </div>
     )
 }

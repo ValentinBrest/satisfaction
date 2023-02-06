ce663a6fb3fa9f346931f0c30e8d26d40f15d244
valk <Valiantsin.Kanapatski@epolsoft.com>
2023-02-06 16:22:02 +0300

    feat: add pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import Counter from "./components/Counter"
+import { Link, Route, Routes } from "react-router-dom"
 import './index.scss'
+import AboutPage from "./pages/AboutPage/AboutPage"
+import MainPage from "./pages/MainPage/MainPage"
 
 function App() {
     return (
         <div className="app">
-            Раз Два Три
-            <Counter/>
+            <Link to="/about">О сайте</Link>
+            <Link to="/main">Главная</Link>
+                <Routes>
+                    <Route path='/about' element={<AboutPage/>}/>
+                    <Route path='/main' element={<MainPage/>}/>
+                </Routes>
         </div>
     )
 }

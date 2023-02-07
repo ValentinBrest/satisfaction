356f52075818b7c52c38aecfbc9cae262627945c
valk <Valiantsin.Kanapatski@epolsoft.com>
2023-02-07 15:16:51 +0300

    feat: add helper for cLassName

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from "react"
 import { Link, Route, Routes } from "react-router-dom"
+import { classNames } from "./helpers/classNames/classNames"
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async"
 import { MainPageAsync } from "./pages/MainPage/MainPage.async"
 import './styles/index.scss'
@@ -10,7 +11,7 @@ function App() {
     const {theme, toggleTheme} = useTheme()
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>TOGGLE</button>
             <Link to="/about">О сайте</Link>
             <Link to="/main">Главная</Link>

96c3ffa4e3e10e1369a700728e40a7089ea3f699
valk <Valiantsin.Kanapatski@epolsoft.com>
2023-02-07 11:23:54 +0300

    feat: create hook useTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-import { Suspense, useContext, useState } from "react"
+import { Suspense } from "react"
 import { Link, Route, Routes } from "react-router-dom"
-import './styles/index.scss'
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async"
 import { MainPageAsync } from "./pages/MainPage/MainPage.async"
-import { Theme, ThemeContext } from "./theme/ThemeContext"
+import './styles/index.scss'
+import { useTheme } from "./theme/useTheme"
 
 
 function App() {
-    const  {theme, setTheme} = useContext(ThemeContext);
-    
-    const toggleTheme = () => {
-        setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK)
-    }
+    const {theme, toggleTheme} = useTheme()
 
     return (
         <div className={`app ${theme}`}>

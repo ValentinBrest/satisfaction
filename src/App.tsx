3b5737fe35b0eaaf1f69ece94a19feec35ac1312
valk <Valiantsin.Kanapatski@epolsoft.com>
2023-02-07 10:33:55 +0300

    style: fix index.scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Suspense } from "react"
 import { Link, Route, Routes } from "react-router-dom"
-import './index.scss'
+import './styles/index.scss'
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async"
 import { MainPageAsync } from "./pages/MainPage/MainPage.async"
 
 function App() {
     return (
-        <div className="app">
+        <div className="app light">
             <Link to="/about">О сайте</Link>
             <Link to="/main">Главная</Link>
             <Suspense fallback={<div>Loading...</div>}>

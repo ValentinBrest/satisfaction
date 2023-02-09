b83984ba3029a63572db5561b38f57b88d423b3b
valk <Valiantsin.Kanapatski@epolsoft.com>
2023-02-09 08:53:21 +0300

    refactor: modify routeConfig

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -6,7 +6,7 @@ const AppRouter = () => {
     return ( 
         <Suspense fallback={<div>Loading...</div>}>
             <Routes>
-                {Object.values(routeConfig).map(({path, element}) => (
+                {routeConfig.map(({path, element}) => (
                     <Route 
                         key={path} 
                         path={path} 

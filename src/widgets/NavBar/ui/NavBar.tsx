409babef4cf345e9e1996149a4e7e9ace9492e2a
valk <Valiantsin.Kanapatski@epolsoft.com>
2023-02-10 15:59:21 +0300

    feat: add translation for links

--- src/widgets/NavBar/ui/NavBar.tsx
+++ src/widgets/NavBar/ui/NavBar.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink } from 'shared/ui';
 import { AppLinkTheme } from 'shared/ui/AppLink/AppLink';
@@ -8,12 +9,14 @@ interface NavBarProps {
 }
 
 export const NavBar = ({className}: NavBarProps) => {
+    const {t} = useTranslation()
+
     return (
         <div className={classNames(cl.NavBar, {}, [className])}>
             
             <div className={cl.links}>
-                <AppLink theme={AppLinkTheme.INVERTED} to="/about" className={cl.mainLink}>О сайте</AppLink>
-                <AppLink to="/">Главная</AppLink>
+                <AppLink theme={AppLinkTheme.INVERTED} to="/about" className={cl.mainLink}>{t('Ссылка о сайте')}</AppLink>
+                <AppLink to="/">{t('Ссылка главная')}</AppLink>
             </div>
             
         </div>

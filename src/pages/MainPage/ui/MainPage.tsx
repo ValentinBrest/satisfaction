81b223d4b4faa917222b5ea249f9568178bb25d3
valk <Valiantsin.Kanapatski@epolsoft.com>
2023-02-10 15:26:03 +0300

    feat: add chunk translation

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,7 +1,10 @@
+import { useTranslation } from "react-i18next"
 
 function MainPage() {
+    const {t} = useTranslation('main')
+
     return (
-        <div>MainPage</div>
+        <div>{t('Главная страница')}</div>
     )
 }
 

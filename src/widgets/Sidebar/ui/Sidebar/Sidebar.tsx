4faf3678b941c98700fac1a7ef4359121e48d300
valk <Valiantsin.Kanapatski@epolsoft.com>
2023-02-10 10:04:00 +0300

    feat: create Sidebar

---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import cl from './Sidebar.module.scss'
+
+interface SidebarProps {
+    className?: string;
+}
+
+export const Sidebar = ({className}: SidebarProps) => {
+    const [collapsed, setCollapsed] = useState(false);
+
+    const onToggle = () => {
+        setCollapsed(prev => !prev)
+    }
+    return (
+        <div className={classNames(cl.Sidebar, {[cl.collapsed]: collapsed}, [className])}>
+            <button onClick={onToggle}>Toggle</button>
+        </div>
+    )
+}

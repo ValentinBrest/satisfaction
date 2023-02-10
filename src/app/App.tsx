c41edf5b3fe05e02b49c231dff58908344577673
valk <Valiantsin.Kanapatski@epolsoft.com>
2023-02-10 14:52:28 +0300

    feat: create Translate component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,6 @@
 import { useTheme } from "app/providers/ThemeProvider"
+import { Suspense } from "react"
+import { useTranslation } from "react-i18next"
 import { classNames } from "shared/lib/classNames/classNames"
 import { NavBar } from "widgets/NavBar"
 import { Sidebar } from "widgets/Sidebar/ui"
@@ -6,16 +8,35 @@ import { AppRouter } from "./providers/router"
 import './styles/index.scss'
 
 
+const Component = () => {
+    const { t, i18n } = useTranslation();
+
+    const toggleLng = () => {
+        i18n.changeLanguage(i18n.language === 'en' ? 'ru': 'en')
+    }
+
+    return (
+        <div>
+            <button onClick={toggleLng}>{t('Перевод')}</button>
+            {t('Тестовый пример')}
+        </div>
+    )
+}
+
 function App() {
     const {theme} = useTheme()
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <NavBar/>
-            <div className="content-page">
-                <Sidebar/>
-                <AppRouter/>  
-            </div>
+            <Suspense fallback=''>
+                <Component/>
+                <NavBar/>
+                <div className="content-page">
+                    <Sidebar/>
+                    <AppRouter/>  
+                </div>
+            </Suspense>
+            
         </div>
     )
 }

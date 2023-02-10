19e5b0cca8df6d30ddd54a8fa1a39902193fc012
valk <Valiantsin.Kanapatski@epolsoft.com>
2023-02-10 10:45:05 +0300

    style: styling Sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import { useTheme } from "app/providers/ThemeProvider"
 import { classNames } from "shared/lib/classNames/classNames"
 import { NavBar } from "widgets/NavBar"
+import { Sidebar } from "widgets/Sidebar/ui"
 import { AppRouter } from "./providers/router"
 import './styles/index.scss'
 
@@ -11,7 +12,10 @@ function App() {
     return (
         <div className={classNames('app', {}, [theme])}>
             <NavBar/>
-            <AppRouter/>    
+            <div className="content-page">
+                <Sidebar/>
+                <AppRouter/>  
+            </div>
         </div>
     )
 }

a3f8b9872f21e6c507a214ed516dd2f2b6809449
valk <Valiantsin.Kanapatski@epolsoft.com>
2023-02-15 11:31:30 +0300

    docs: add jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,9 @@ module.exports = {
     env: {
         'browser': true,
         'es2021': true,
+        'jest': true,
     },
     extends: [
-        'eslint:recommended',
         'plugin:react/recommended',
         'plugin:@typescript-eslint/recommended',
     ],

54dfe9bf7bae1ce1a5301405e58cede3a43976c0
Valentin Konopatsky <fixator03@yandex.ru>
2023-02-18 14:33:04 +0300

    docs: add storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     extends: [
         'plugin:react/recommended',
         'plugin:@typescript-eslint/recommended',
+        'plugin:storybook/recommended',
     ],
     parser: '@typescript-eslint/parser',
     parserOptions: {

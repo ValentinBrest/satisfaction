d0b3899c4222a773359a00aed3c6654b1ccc1294
Valentin Konopatsky <fixator03@yandex.ru>
2023-02-19 12:51:42 +0300

    story: create NavBar.stories

--- src/widgets/NavBar/ui/NavBar.stories.tsx
+++ src/widgets/NavBar/ui/NavBar.stories.tsx
@@ -2,17 +2,17 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { Theme } from 'app/providers/ThemeProvider';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 
-import { Sidebar } from './Sidebar';
+import { NavBar } from './NavBar';
 
 export default {
-    title: 'widget/Sidebar',
-    component: Sidebar,
+    title: 'widget/NavBar',
+    component: NavBar,
     argTypes: {
         backgroundColor: { control: 'color' },
     },
-} as ComponentMeta<typeof Sidebar>;
+} as ComponentMeta<typeof NavBar>;
 
-const Template: ComponentStory<typeof Sidebar> = (args) => <Sidebar {...args} />;
+const Template: ComponentStory<typeof NavBar> = (args) => <NavBar {...args} />;
 
 export const Light = Template.bind({});
 Light.args = {};
@@ -20,3 +20,4 @@ Light.args = {};
 export const Dark = Template.bind({});
 Dark.args = {};
 Dark.decorators= [ThemeDecorator(Theme.DARK)];
+

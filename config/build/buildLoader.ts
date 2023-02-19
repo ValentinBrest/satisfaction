98e1d1f51d37f46d47f0d484b214ffc0cf56917e
Valentin Konopatsky <fixator03@yandex.ru>
2023-02-19 09:57:50 +0300

    feat: override webpack.config for storybook

--- config/build/buildLoader.ts
+++ config/build/buildLoader.ts
@@ -1,5 +1,6 @@
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
+
+import { buildCssLoaders } from './loaders/buildCssLoaders';
 
 export function buildLoader (isDev: boolean): webpack.RuleSetRule[] {
     const babelLoader = {
@@ -27,29 +28,7 @@ export function buildLoader (isDev: boolean): webpack.RuleSetRule[] {
         ],
     };
   
-    const cssLoader = {
-        test: /\.s[ac]ss$/i,
-        use: [
-            // Creates `style` nodes from JS strings
-            isDev ? 'style-loader': MiniCssExtractPlugin.loader,
-            // Translates CSS into CommonJS
-            {
-                loader: 'css-loader',
-                options: {
-                    modules: {
-                        auto: (
-                            (resourcePath: string) => Boolean(resourcePath.includes('.module.'))
-                        ),
-                        localIdentName: isDev
-                            ? '[path][name]__[local]--[hash:base64:5]'
-                            : '[hash:base64:8]',
-                    },
-                },
-            },
-            // Compiles Sass to CSS
-            'sass-loader',
-        ],
-    };
+    const cssLoader = buildCssLoaders(isDev);
   
     const tsLoader = {
         test: /\.tsx?$/,

946d75fc4de76dcf681cbae2547e17b5b182a3c6
valk <Valiantsin.Kanapatski@epolsoft.com>
2023-02-22 15:26:40 +0300

    fix: fix mistakes with themes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,15 @@
 import { Suspense } from 'react';
-import { useTheme } from 'app/providers/ThemeProvider';
-import { classNames } from 'shared/lib/classNames/classNames';
 import { NavBar } from 'widgets/NavBar';
 import { Sidebar } from 'widgets/Sidebar/ui';
 
 import { AppRouter } from './providers/router';
 
-
-
 function App() {
-    const {theme} = useTheme();
 
     return (
-        <div className={classNames('app', {}, [theme])}>
+        <div className="app">
             <Suspense fallback="">
                 <NavBar/>
-                
-                
                 <div className="content-page">
                     <Sidebar/>
                     <AppRouter/>  

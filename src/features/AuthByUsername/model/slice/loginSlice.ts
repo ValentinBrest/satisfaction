0b3f042f72d7511e14ccefdb47e447d1a301bd1b
valk <Valiantsin.Kanapatski@epolsoft.com>
2023-02-27 11:20:56 +0300

    feat: create slice, types and selectors for AuthByUserName

---
+++ src/features/AuthByUsername/model/slice/loginSlice.ts
@@ -0,0 +1,25 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+import { LoginSchema } from '../types/LoginSchema';
+
+const initialState: LoginSchema = {
+    username: '',
+    password: '',
+    isLoading: false,
+};
+
+export const loginSlice = createSlice({
+    name: 'login',
+    initialState,
+    reducers: {
+        setUsername: (state, action: PayloadAction<string>) => {
+            state.username = action.payload;
+        },
+        setPassword: (state, action) => {
+            state.password = action.payload;
+        },
+    },
+});
+
+export const { actions: loginActions } = loginSlice;
+export const { reducer: loginReducer } = loginSlice;
--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -1,8 +1,13 @@
+import { memo, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useDispatch, useSelector } from 'react-redux';
 import EmailIcon from 'shared/assets/icons/auth/email.svg';
 import PasswordIcon from 'shared/assets/icons/auth/password.svg';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Button, ButtonTheme, Input } from 'shared/ui';
+
+import { getLoginState } from '../../model/selectors/getLoginState/getLoginState';
+import { loginActions } from '../../model/slice/loginSlice';
 
 import cl from './LoginForm.module.scss';
 
@@ -11,17 +16,41 @@ interface LoginFormProps {
     isOpen?: boolean; 
 }
 
-export const LoginForm = ({className, isOpen}: LoginFormProps) => {
+export const LoginForm = memo(({className, isOpen}: LoginFormProps) => {
     const {t} = useTranslation();
+    const dispatch = useDispatch();
+    const {username, password} = useSelector(getLoginState);
+
+    const onChangeUsername = useCallback((value: string) => {
+        dispatch(loginActions.setUsername(value));
+    }, [dispatch]);
+
+    const onChangePassword = useCallback((value: string) => {
+        dispatch(loginActions.setPassword(value));
+    }, [dispatch]);
+
     return (
         <div className={classNames(cl.LoginForm, {}, [className])}>
             <div className={cl.wrapInput}>
                 <EmailIcon className={cl.icon}/>
-                <Input className={cl.input} type="text" placeholder="Email" isOpen={isOpen}/>
+                <Input 
+                    className={cl.input} 
+                    type="text" 
+                    placeholder="Email" 
+                    isOpen={isOpen} 
+                    onChange={onChangeUsername}
+                    value={username}
+                />
             </div>
             <div className={cl.wrapInput}>
                 <PasswordIcon className={cl.icon}/>
-                <Input className={cl.input} type="password" placeholder="Password"/>
+                <Input 
+                    className={cl.input} 
+                    type="password" 
+                    placeholder="Password" 
+                    onChange={onChangePassword}
+                    value={password}
+                />
             </div>
             
             <Button 
@@ -32,4 +61,4 @@ export const LoginForm = ({className, isOpen}: LoginFormProps) => {
             </Button>
         </div>
     );
-};
+});

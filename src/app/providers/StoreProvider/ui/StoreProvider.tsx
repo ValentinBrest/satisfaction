a517dc90aba7f5691d28b2b80e60a00c09906785
valk <Valiantsin.Kanapatski@epolsoft.com>
2023-03-01 15:31:56 +0300

    fix: fix mistakes in storybook adding asyncReducers

--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -1,17 +1,26 @@
 import { ReactNode } from 'react';
 import { Provider } from 'react-redux';
-import { DeepPartial } from '@reduxjs/toolkit';
+import { DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
 import { createReduxStore } from 'app/providers/StoreProvider';
 import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema';
 
 interface StoreProviderProps {
     children: ReactNode;
     initialState?: DeepPartial<StateSchema>;
+    asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>
 }
 
-export const StoreProvider = ({children, initialState}: StoreProviderProps) => {
-   
-    const store = createReduxStore(initialState as StateSchema);
+export const StoreProvider = (props: StoreProviderProps) => {
+    const {
+        children, 
+        initialState, 
+        asyncReducers,
+    } = props;
+
+    const store = createReduxStore(
+        initialState as StateSchema, 
+        asyncReducers as ReducersMapObject<StateSchema>,
+    );
 
     return (
         <Provider store={store}>

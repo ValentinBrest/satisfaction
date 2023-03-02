11ce75656e30de7d4b33a82529d56e1bd8ccaef5
valk <Valiantsin.Kanapatski@epolsoft.com>
2023-03-02 10:08:57 +0300

    test: create tests for AuthByUsername

---
+++ src/features/AuthByUsername/model/selectors/getLoginIsLoading/getLoginIsLoading.test.tsx
@@ -0,0 +1,15 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+
+import { getLoginIsLoading } from './getLoginIsLoading';
+
+describe('getLoginIsLoading', () => {
+    test('should return true', () => {
+        const state: DeepPartial<StateSchema> = {loginForm: {isLoading: true}};
+        expect(getLoginIsLoading(state as StateSchema)).toEqual(true);
+    });
+    test('should with work state', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getLoginIsLoading(state as StateSchema)).toEqual(false);
+    });
+});
---
+++ src/features/AuthByUsername/model/selectors/getLoginUsername/getLoginUsername.test.tsx
@@ -0,0 +1,15 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+
+import { getLoginUsername } from './getLoginUsername';
+
+describe('getLoginUsername', () => {
+    test('should return username', () => {
+        const state: DeepPartial<StateSchema> = {loginForm: {username: 'valk'}};
+        expect(getLoginUsername(state as StateSchema)).toEqual('valk');
+    });
+    test('should with work state', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getLoginUsername(state as StateSchema)).toEqual('');
+    });
+});

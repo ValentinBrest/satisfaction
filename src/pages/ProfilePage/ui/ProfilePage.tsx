2e49038de0d1f8d32904781d8c1c068f3e79a032
valk <Valiantsin.Kanapatski@epolsoft.com>
2023-03-03 12:27:06 +0300

    feat: create Profile in entities

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,12 +1,18 @@
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
+import { profileReducer } from 'entities/Profile';
+import { DynamicModuleLoader, ReducerList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+
+const reducers: ReducerList = {
+    profile: profileReducer,
+};
 
 const ProfilePage = memo(() => {
     const {t} = useTranslation('profile');
     return (
-        <>
+        <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
             <div>{t('profilePage')}</div>
-        </>
+        </DynamicModuleLoader>
     );
 });
 
---
+++ src/shared/const/common.ts
@@ -0,0 +1,13 @@
+export enum Currency {
+    RUB = 'RUB',
+    BYN = 'BYN',
+    EUR = 'EUR',
+    USD = 'USD'
+}
+
+export enum Country {
+    Russia = 'Russia',
+    Belarus = 'Belarus',
+    Ukraine = 'Ukraine',
+    Poland = 'Poland'
+}

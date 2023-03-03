f407387509e36af6bf97cf87515de351ef83d94f
valk <Valiantsin.Kanapatski@epolsoft.com>
2023-03-03 16:29:07 +0300

    feat: add extraReducers in profileSlice.ts

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,13 +1,20 @@
-import { memo } from 'react';
+import { memo, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import { profileReducer } from 'entities/Profile';
+import { fetchProfileData, profileReducer } from 'entities/Profile';
 import { DynamicModuleLoader, ReducerList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 
 const reducers: ReducerList = {
     profile: profileReducer,
 };
 
 const ProfilePage = memo(() => {
+    const dispatch = useAppDispatch();
+
+    useEffect (() => {
+        dispatch(fetchProfileData());
+    }, [dispatch]);
+
     const {t} = useTranslation('profile');
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount>

173173e780c889e001a3a5b49538541129aea538
valk <Valiantsin.Kanapatski@epolsoft.com>
2023-03-06 11:32:45 +0300

    fix: fix mistake in storybook

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -15,8 +15,9 @@ export default ({config}: {config: webpack.Configuration}) => {
     config.resolve?.modules?.push(paths.src);
     config.resolve?.extensions?.push('.ts', '.tsx');
 
-    // eslint-disable-next-line no-param-reassign
-    config.module.rules = config.module?.rules?.map((rule: RuleSetRule) => {
+    const rules = config.module!.rules as RuleSetRule[];
+
+    config.module!.rules = rules.map((rule) => {
         if (/svg/.test(rule.test as string)) {
             return { ...rule, exclude: /\.svg$/i };
         }

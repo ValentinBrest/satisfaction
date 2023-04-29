2b94fdcf2bcf59a4fbe374fd62e2072339a8b85c
Valentin Konopatsky <fixator03@yandex.ru>
2023-04-30 01:31:08 +0300

    docs: add concerrently and Folder Tepmlates Plugin

---
+++ .fttemplates/TS Component/[FTName].tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+
+import cl from './[FTName].module.scss';
+
+interface [FTName]Props {
+   className?: string;
+}
+
+export const [FTName]: FC<[FTName]Props> = (props) => {
+   const { className } = props;
+   const { t } = useTranslation()
+
+   return (
+      <div className={classNames(cl.[FTName], {}, [className])}>
+
+      </div>
+   );
+}

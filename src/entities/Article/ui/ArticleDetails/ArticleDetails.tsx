37098fc0df4f5ff9d029b20ecb0a2360d3edf2b7
Valentin Konopatsky <fixator03@yandex.ru>
2023-05-07 17:40:07 +0300

    feat: create slice for Article

--- src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
@@ -1,5 +1,10 @@
 import { useTranslation } from 'react-i18next';
+import { articleDetailsReducer } from 'entities/Article/model/slice/articleDetailsSlice';
 import { classNames } from 'shared/lib/classNames/classNames';
+import {
+    DynamicModuleLoader,
+    ReducerList,
+} from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 
 import cl from './ArticleDetails.module.scss';
 
@@ -7,11 +12,19 @@ interface ArticleDetailsProps {
     className?: string;
 }
 
+const reducers: ReducerList = {
+    articleDetails: articleDetailsReducer,
+};
+
 export const ArticleDetails = (props: ArticleDetailsProps) => {
     const { className } = props;
     const { t } = useTranslation();
 
     return (
-        <div className={classNames(cl.ArticleDetails, {}, [className])}></div>
+        <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
+            <div
+                className={classNames(cl.ArticleDetails, {}, [className])}
+            ></div>
+        </DynamicModuleLoader>
     );
 };

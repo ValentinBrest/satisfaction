051e189d4159e974d5c9b8c4b85162abb3cc62b7
valk <Valiantsin.Kanapatski@epolsoft.com>
2023-05-15 18:17:25 +0300

    feat: create selectors Article

--- src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
@@ -1,5 +1,6 @@
 import { memo, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
 import { articleDetailsReducer } from 'entities/Article/model/slice/articleDetailsSlice';
 import { classNames } from 'shared/lib/classNames/classNames';
 import {
@@ -8,6 +9,11 @@ import {
 } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 
+import { 
+    getArticleDetailsData, 
+    getArticleDetailsError, 
+    getArticleDetailsIsLoading,
+} from '../../model/selectors/articleDetails';
 import { fetchArticleById } from '../../model/services/fetchArticleById/fetchArticleById';
 
 import cl from './ArticleDetails.module.scss';
@@ -25,16 +31,36 @@ export const ArticleDetails = memo((props: ArticleDetailsProps) => {
     const { className, id } = props;
     const { t } = useTranslation();
     const dispatch = useAppDispatch();
+    const isLoading = useSelector(getArticleDetailsIsLoading);
+    const article = useSelector(getArticleDetailsData);
+    const error = useSelector(getArticleDetailsError);
 
     useEffect(() => {
         dispatch(fetchArticleById(id));
     }, [dispatch, id]);
 
+    let content;
+
+    if (error) {
+        // eslint-disable-next-line i18next/no-literal-string
+        content = <div>Error</div>;
+    } else if (isLoading) {
+        // eslint-disable-next-line i18next/no-literal-string
+        content = <div>...Loading</div>;
+    } else {
+        // eslint-disable-next-line i18next/no-literal-string
+        content = <div>Article Details</div>;
+    }
+
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
             <div
                 className={classNames(cl.ArticleDetails, {}, [className])}
-            ></div>
+            >
+                {content}
+            </div>
         </DynamicModuleLoader>
     );
 });
+
+

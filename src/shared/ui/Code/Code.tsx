dc9f72e9c58d8eb0e7ddaf7c82c2c9926d874433
valk <Valiantsin.Kanapatski@epolsoft.com>
2023-05-31 12:48:26 +0300

    feat: create component Code in shared

---
+++ src/shared/ui/Code/Code.tsx
@@ -0,0 +1,33 @@
+import { memo, useCallback } from 'react';
+import CopyIcon from 'shared/assets/icons/article/copy.svg';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+
+import cl from './Code.module.scss';
+
+interface CodeProps {
+    className?: string;
+    text: string;
+}
+
+export const Code = memo((props: CodeProps) => {
+    const {
+        className, 
+        text,
+    } = props;
+
+    const onCopy = useCallback(() => {
+        navigator.clipboard.writeText(text);
+    }, [text]);
+
+    return (
+        <pre className={classNames(cl.Code, {}, [className])}>
+            <Button onClick={onCopy} className={cl.copyBtn} theme={ButtonTheme.CLEAR}>
+                {<CopyIcon/>}
+            </Button>
+            <code>
+                {text}
+            </code>
+        </pre>
+    );
+});

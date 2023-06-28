93e4f7b769fbc2d32500ccb9e281450a89328e32
valk <Valiantsin.Kanapatski@epolsoft.com>
2023-06-28 18:15:39 +0300

    feat: add button back for ArticleDetailsPage

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -1,15 +1,17 @@
 import { FC, memo, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { RoutePath } from 'app/providers/router/routeConfig/routeConfig';
 import { ArticleDetails } from 'entities/Article';
 import { CommentList } from 'entities/Comment';
 import { AddCommentForm } from 'features/addComentForm';
+import BackIcon from 'shared/assets/icons/back.svg';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { DynamicModuleLoader, ReducerList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
-import { Text, TextSize } from 'shared/ui';
+import { Button, ButtonTheme, Text, TextSize } from 'shared/ui';
 
 import { getArticleCommentsError, getArticleCommentsIsLoading } from '../../model/selectors/comments';
 import { addCommentForArticle } from '../../model/services/addCommentForArticle/addCommentForArticle';
@@ -34,12 +36,17 @@ const ArticleDetailsPage: FC<ArticleDetailsPageProps> = (props) => {
     const comments = useSelector(getArticleComments.selectAll);
     const commentsIsLoading = useSelector(getArticleCommentsIsLoading);
     const commentsError = useSelector(getArticleCommentsError);
-
+    const navigate = useNavigate();
+    
     const onSendComment = useCallback((text: string) => {
         dispatch(addCommentForArticle(text));
     }, [dispatch]);
 
     useInitialEffect(() => dispatch(fetchCommentsByArticleById(id)));
+    
+    const onBackToList = useCallback(() => {
+        navigate(RoutePath.articles);
+    }, [navigate]);
 
     if (!id) {
         return (
@@ -52,6 +59,9 @@ const ArticleDetailsPage: FC<ArticleDetailsPageProps> = (props) => {
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
             <div className={classNames(cl.ArticleDetailsPage, {}, [className])}>
+                <Button onClick={onBackToList} theme={ButtonTheme.CLEAR} className={cl.icon}>
+                    <BackIcon/>
+                </Button>
                 <ArticleDetails id={id}/>
                 <Text title={t('kommentarii')} size={TextSize.L}/>
                 <AddCommentForm onSendComment={onSendComment}/>

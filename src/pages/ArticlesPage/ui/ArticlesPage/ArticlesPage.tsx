7803b0b00548010db94346e8446a40fd4f169de2
valk <Valiantsin.Kanapatski@epolsoft.com>
2023-06-28 18:17:35 +0300

    feat: improve ActiclePage

--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -1,6 +1,9 @@
 import { FC, memo } from 'react';
 import { useTranslation } from 'react-i18next';
+import { ArticleList } from 'entities/Article';
+import { article } from 'entities/Article/mocks/data';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { Text, TextSize } from 'shared/ui';
 
 import cl from './ArticlesPage.module.scss';
 
@@ -13,7 +16,10 @@ const ArticlesPage: FC<ArticlesPageProps> = (props) => {
     const { t } = useTranslation('articles');
 
     return <div className={classNames(cl.ArticlesPage, {}, [className])}>
-        <div>{t('articles')}</div>
+        <Text title={t('articles')} size={TextSize.L} className={cl.title}/>
+        <ArticleList articles={new Array(16)
+            .fill(0)
+            .map((item, index) => ({...article, id: String(index)}))}/>
     </div>;
 };
 

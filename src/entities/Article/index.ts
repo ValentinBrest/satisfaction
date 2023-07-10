e57ee54a06489bf5cbda9b5feda4ae4565e21b68
valk <Valiantsin.Kanapatski@epolsoft.com>
2023-07-10 14:33:55 +0300

    feat: add types for ArticlesPage

--- src/entities/Article/index.ts
+++ src/entities/Article/index.ts
@@ -2,6 +2,8 @@ export {ArticleDetails} from './ui/ArticleDetails/ArticleDetails';
 export {getArticleDetailsData} from './model/selectors/articleDetails';
 export {ArticleList} from './ui/ArticleList/ArticleList';
 
-export type {Article, ArticleView} from './model/types/article';
+export {ArticleView} from './model/types/article';
+
+export type {Article} from './model/types/article';
 
 export type {ArticleDetailsSchema} from './model/types/articleDetailsSchema';

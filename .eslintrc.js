56a3f734b7367336258d68e2f2b8abfc99ab2eeb
Valentin Konopatsky <fixator03@yandex.ru>
2024-07-17 10:39:49 +0300

    feat: create TS Structure with node

--- .eslintrc.js
+++ .eslintrc.js
@@ -82,7 +82,7 @@ module.exports = {
             },
         },
         {
-            files: ['scripts/*.js', 'json-server/*.js'],
+            files: ['scripts/**/*.js', 'json-server/*.js'],
             rules: {
                 '@typescript-eslint/no-var-requires': 'off',
             },

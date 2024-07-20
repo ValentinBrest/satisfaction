06ae44ece2575594f322e1059afeb3acfd333723
Valentin Konopatsky <fixator03@yandex.ru>
2024-07-20 11:02:11 +0300

    feat: add jest-html-reporters

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -48,6 +48,15 @@ export default {
         '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
     ],
     rootDir: '../../',
+    'reporters': [
+        'default',
+        ['jest-html-reporters', {
+            'publicPath': '<rootDir>/reports/unit',
+            'filename': 'report.html',
+            'openReport': true,
+            'inlineSource': true,
+        }],
+    ],
     // Indicates whether the coverage information should be collected while executing the test
     // collectCoverage: false,
 
